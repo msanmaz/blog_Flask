e15caf7453c4b74455482ef5d5944cd29f2ddcb3
Osanmaz <osanmaz4992@gmail.com>
2020-04-13 04:58:56 +0100

    go to user login

--- app/models.py
+++ app/models.py
@@ -1,11 +1,24 @@
+from datetime import datetime
 from app import db
 
 class User(db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(62), index=True, unique=True)
-    email = db.Column(db.String(120), index=True)
+    username = db.Column(db.String(62), index=True, unique=True,nullable=False)
+    email = db.Column(db.String(120), index=True, unique=True)
     password_has = db.Column(db.String(50))
+    posts = db.relationship('Post', backref='author', lazy='dynamic')
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
+
+
+class Post(db.Model):
+    __tablename__='post'
+    id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.String(140))
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+
+    def __repr__(self):
+        return '<Post {}>'.format(self.body)
