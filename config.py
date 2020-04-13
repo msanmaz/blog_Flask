1200e1a1dece7d88770de7d187a2666d76363197
Osanmaz <osanmaz4992@gmail.com>
2020-04-13 02:57:06 +0100

    config database

--- config.py
+++ config.py
@@ -1,4 +1,15 @@
 import os
-
+basedir = os.path.abspath(os.path.dirname(__file__))
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+    SQLALCHEMY_DATABASE_URI = 'postgres://m@localhost:5432/microblog'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+
+class StatingConfig(Config):
+    DEVELOPMENT = True
+    DEBUG = True
+
+class DevelopmentConfig(Config):
+    DEVELOPMENT = True
+    DEBUG = True
