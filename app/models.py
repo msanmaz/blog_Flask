fa35d462b22e18c18f2c93810f38e42d22f5fe3d
Osanmaz <osanmaz4992@gmail.com>
2020-04-13 06:30:34 +0100

    login validation using Flask-Form

--- app/models.py
+++ app/models.py
@@ -1,13 +1,28 @@
 from datetime import datetime
-from app import db
+from app import db , login
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
 
-class User(db.Model):
+
+
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
+
+class User(UserMixin, db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(62), index=True, unique=True,nullable=False)
     email = db.Column(db.String(120), index=True, unique=True)
     password_has = db.Column(db.String(50))
     posts = db.relationship('Post', backref='author', lazy='dynamic')
+
+
+    def set_password(self, password):
+        self.password_has = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_has,password)
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
@@ -22,3 +37,5 @@ class Post(db.Model):
 
     def __repr__(self):
         return '<Post {}>'.format(self.body)
+
+
