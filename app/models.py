80be0ede3dae74f41e0a054c238318812d356ea7
Osanmaz <osanmaz4992@gmail.com>
2020-04-27 13:20:48 +0100

    adding avatar for users

--- app/models.py
+++ app/models.py
@@ -40,5 +40,10 @@ class Post(db.Model):
 
 
 class Likes(db.Model):
+    __tablename__='likes'
     id = db.Column(db.Integer, primary_key=True)
-    user_id
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    post_id = db.Column(db.Integer)
+    def __repr__(self):
+        return '<Post {}>'.format(self.post_id)
+
